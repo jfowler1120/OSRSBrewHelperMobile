f2d054a864001e7cb4cccd48f15e98c3342d2d11
jfowler1120 <jfowler1120@gmail.com>
2023-12-12 18:03:23 +0000

    First commit
    Beginnings of app UI.

---
+++ OSRSBrewHelper/OSRSBrewHelper/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using OSRSBrewHelper.ViewModels;
+using OSRSBrewHelper.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace OSRSBrewHelper
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ OSRSBrewHelper/OSRSBrewHelper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OSRSBrewHelper.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace OSRSBrewHelper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
